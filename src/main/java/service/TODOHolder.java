ba5e8a3054bfd8812cd4a1dca11b8f3e9e76c389
Victor Tanasa <victor.tanasa@muso.com>
2020-03-28 00:51:33 +0200

    todos

--- src/main/java/service/TODOHolder.java
+++ src/main/java/service/TODOHolder.java
@@ -2,16 +2,7 @@ package service;
 
 public class TODOHolder {
 
-    //TODO: request shelves
-    //https://www.goodreads.com/shelf/list.xml?key=aB9VcY1rOGCzxMONqjk8Ug&user_id=60626198&page=1
-
-    //TODO: get book_count from read shelve
-    //TODO: configure page=1,200&per_page=200 in order to get all books
-    //https://www.goodreads.com/review/list?v=2&key=aB9VcY1rOGCzxMONqjk8Ug&id=60626198&shelf=read&page=1,200&per_page=200
-
     //TODO: idea - if can't get isbn from edition, try others
-
-    //TODO: get books from DNF
 
     //TODO: missing dates thread: https://www.goodreads.com/topic/show/19351061-missing-started-at-and-read-at
 
@@ -21,13 +12,10 @@ public class TODOHolder {
 //    Authors: [Dennis E. Taylor]
 //    Missing Fields: [FORMAT, PAGE_NUMBER]
 
-    //TODO: extract user id as token -> save stats per token
-
-    //TODO: //review/list?v=2&key=aB9VcY1rOGCzxMONqjk8Ug&id=748293&shelf=read&page=1&per_page=580 get number of results, divide by total -> do more requests
-
     //TODO: replace UserShelve with Pair? same for ShelveMapping
 
     //TODO: pages read per month -> pages / days if month finished != month started
 
     //TODO: something to show what stats are available for given data.
+    //TODO: guard against NPE for metrics
 }
